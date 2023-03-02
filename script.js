f31df9846b8254f2683fab9303b738ad01613793
JEREMIAH GONZALEZ <>
2023-03-02 14:40:01 +0000

    unit 3 project

---
+++ script.js
@@ -0,0 +1,36 @@
+                
+let story-opening 
+let optionOnebutton = document.querySelector(".option-one");
+let optionTwobutton = document.querySelector(".option-two");
+let optionOneScreen =document.querySelector(".choose your shoe");
+let optionTwoscreen=document.querySelector(".score");
+let title=document.querySelector("h1");
+let background=document.querySelector("body");
+let jumpButton=document.querySelector(".jump");
+let jumpdownButton=document.querySelector(".jump-down");
+let changeColor=document.querySelector(".change-color");
+let endGame=document.querySelector(".end-game");
+let updateScore=document.querySelector(".update-score");
+let grow=document.querySelector(".grow");
+
+
+
+
+
+
+When you're ready to make event handlers, uncomment the code below. Then fill in the blanks with the correct variables!
+INSERT_VARIABLE.onclick=function(){
+optionOnebutton.onclick=function
+
+
+
+INSERT_VARIABLE.onclick=function(){
+optionTwobutton.onclick=function
+};
+
+
+INSERT_VARIABLE.onclick=function(){
+
+};
+
+*/
